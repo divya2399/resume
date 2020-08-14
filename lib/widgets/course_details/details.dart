d7447f08d808671f00ea25aa269f621ed3a99643
divya2399 <53622635+divya2399@users.noreply.github.com>
2020-08-14 12:49:23 +0530

    First Commit

--- lib/widgets/course_details/details.dart
+++ lib/widgets/course_details/details.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
-
+// ignore: avoid_web_libraries_in_flutter
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'dart:html' as html;
 class Details extends StatelessWidget {
   const Details({Key key}) : super(key: key);
 
@@ -49,7 +51,52 @@ class Details extends StatelessWidget {
                   color: Colors.white
                 ),
                 textAlign: TextAlign.left,
-              )
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  RaisedButton(
+                    shape: StadiumBorder(),
+                    child: Text("Resume", style: TextStyle(color: Colors.white),),
+                    color: Color(0xFF01579B),
+                    onPressed: () {
+                      final url = "assets/PDF.pdf";
+                      final anchor = html.document.createElement('a') as html.AnchorElement
+                        ..href = url
+                        ..style.display = 'none'
+                        ..download = 'Resume.pdf';
+                      html.document.body.children.add(anchor);
+                      anchor.click();
+                      html.document.body.children.remove(anchor);
+                    },
+                    padding: EdgeInsets.all(10),
+                  ),
+                  SizedBox(
+                    width: 35,
+                  ),
+                  RaisedButton(
+                    //padding: EdgeInsets.all(6),
+                    color: Colors.black,
+                    child: FaIcon(FontAwesomeIcons.github,color: Colors.white,),
+                    onPressed: () {
+                      html.window.open("https://github.com/divya2399", "Git");
+                    },
+                    //color: Colors.blue,
+                    shape: StadiumBorder(),
+                  ),
+                  RaisedButton(
+                    color: Colors.black,
+                    child: FaIcon(FontAwesomeIcons.linkedinIn,color: Colors.white,),
+                    onPressed: () {
+                      html.window.open("https://www.linkedin.com/in/divya234", "LinkedIn");
+                    },
+                    shape: StadiumBorder(),
+                  ),
+                ],
+              ),
             ],
           ),
         );
